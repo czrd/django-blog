6d7fbcc486697fd38ec650fe472c29e68e925d08
Takeshi Itami <tksh.itm@gmail.com>
2020-06-24 19:10:33 +0900

    add post_share view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -19,3 +20,20 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # ...send email
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {
+        'post': post,
+        'form': form
+    })
